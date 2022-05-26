1434836cc46037c8c39a6053e170bf84b1736f43
João Matos - a22003420 <jpcamatos31@gmail.com>
2022-05-26 19:49:46 +0100

    final_changes_heroku_1

--- config/urls.py
+++ config/urls.py
@@ -22,13 +22,14 @@ from django.conf.urls.static import static
 from django.contrib.staticfiles.storage import staticfiles_storage
 from django.views.generic.base import RedirectView
 
+favicon_view = RedirectView.as_view(url='/static/favicon.ico', permanent=True)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('portfolio.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
+    path(r'^favicon\.ico$', favicon_view),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
