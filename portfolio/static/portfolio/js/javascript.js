989e7c2c6cb8a1625ac623e75fd92cf236de5d6c
João Matos - a22003420 <jpcamatos31@gmail.com>
2022-06-03 13:10:36 +0100

    update_2

--- portfolio/static/portfolio/js/javascript.js
+++ portfolio/static/portfolio/js/javascript.js
@@ -8,12 +8,33 @@ document.addEventListener('DOMContentLoaded', function () {
         })
         .then(weatherData => {
             console.log(weatherData);
-            let apiData = 'O tempo em ' + weatherData.location.region + ' é de ' + weatherData.current.temperature; 
+            let apiData = 'O tempo em ' + weatherData.location.region + ' é de ' + weatherData.current.temperature + '&#8451;.'; 
             document.getElementById('forecast').innerHTML=apiData;
         })
 
 
 });
 
+document.addEventListener('DOMContentLoaded', function () {
+
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Host': 'random-words5.p.rapidapi.com',
+            'X-RapidAPI-Key': 'ed1c1ef4b8msheec033fa498f060p18a1cfjsn00e4fc492429'
+        }
+    };
+
+    fetch('https://random-words5.p.rapidapi.com/getMultipleRandom?count=5', options)
+    .then(response => {
+        return response.json()
+    })
+        .then(response => {
+            console.log(response);
+            var api = 'Carrega em F5 e várias palavras random irão aparecer a cada pedido &#128512; -> ' + response;
+            document.getElementById('randomWords').innerHTML = api;
+    })
+    
 
 
+});
